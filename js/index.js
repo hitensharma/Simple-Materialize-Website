4b03ff2a7c163fd77a3df9444264bc35411c59fc
Hiten Sharma <hitensharma1515@gmail.com>
2020-05-16 20:37:31 +0530

    start

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.slider').slider({ full_width: true });
+    $('.parallax').parallax();
+    $('.myreviews').carousel({
+        numVisible: 5,
+        shift: 55,
+        padding: 55
+    })
+});
+
+function toggleModal() {
+    var instance = M.Modal.getInstance($('#modal3'))
+    instance.open()
+}
